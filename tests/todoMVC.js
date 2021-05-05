15eb2d663c851b5662976281e1c67cfe01dbdb6d
Rafael Muniz Vieira <rafael.muniz.vieira@gmail.com>
2021-05-05 17:10:40 -0300

    created new tests

--- tests/todoMVC.js
+++ tests/todoMVC.js
@@ -4,9 +4,32 @@ import todopage from './pages/TodoPage';
 fixture("Todo MVC Fixture")
     .page(todopage.URL);
 
-test("Create Todo Test", async t => {
-    await t
+test("Create a todo", async t => {
+
     await todopage.newTodo();
+    await t
+        .expect(todopage.listLength.count).eql(1);
+
+});
+
+test('Create a todo and delete it', async t => {
+
+    await todopage.newTodo();
+    await t
+        .expect(todopage.listLength.count).eql(1)
+        .hover(todopage.item(1))
+        .click(todopage.deleteItem(1))
+        .expect(todopage.listLength.count).eql(0);
+
+});
+
+test('Create a todo and amend it', async t => {
+
+    await todopage.newTodo();
+    await t
+        .expect(todopage.listLength.count).eql(1);
+    await todopage.amendTodo(1);
+
 });
 
 test('Create 10 todos', async t => {
@@ -22,13 +45,13 @@ test('Create 10 todos and delete one by one', async t => {
     await todopage.createTodos(10);
     await t
         .expect(todopage.listLength.count).eql(10)
-        for(var i = 10; i > 0; i--){
-            await t
-                .hover(todopage.item(i))
-                .click(todopage.deleteItem(i));
-        };
+    for (var i = 10; i > 0; i--) {
         await t
-            .expect(todopage.listLength.count).eql(0);
+            .hover(todopage.item(i))
+            .click(todopage.deleteItem(i));
+    };
+    await t
+        .expect(todopage.listLength.count).eql(0);
 
 });
 
@@ -37,8 +60,8 @@ test('Create 10 todos and amend then', async t => {
     await todopage.createTodos(10);
     await t
         .expect(todopage.listLength.count).eql(10)
-        for(var i = 10; i > 0; i--){
-            await todopage.amendTodo(i);
-        };
+    for (var i = 10; i > 0; i--) {
+        await todopage.amendTodo(i);
+    };
 
 });
